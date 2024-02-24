de0dd439ac9b70beb32eb5035f3cf5a30a0655eb
bukey <diana.bukeikhanova@my.jcu.edu.au>
2024-02-24 15:06:01 +0800

    Add prac 1 files

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,17 +1,27 @@
+import random
+
+
 def main():
-    score = float(input("Enter score: "))
-    message = evaluate_score(score)
-    print(message)
+    """Based on score, print score's category."""
+    user_score = float(input("Enter score: "))
+    print(evaluate_score(user_score))
+
+    random_score = random.randint(0, 100)
+    print(f"Your random score: {random_score}\n"
+          f"It is {evaluate_score(random_score)}.")
 
 
-def evaluate_score(estimation):
-    if estimation <= 0 or estimation => 100:
+
+def evaluate_score(score):
+    """Evaluate score's category."""
+    if score <= 0 or score >= 100:
         return "Invalid score"
-    elif estimation >= 90:
+    elif score >= 90:
         return "Excellent"
-    elif estimation >= 50:
+    elif score >= 50:
         return "Pass"
     else:
         return "Bad"
+
 
 main()
